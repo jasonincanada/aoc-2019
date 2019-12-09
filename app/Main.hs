a50d5b8f8214a1fad3996444f59f6fda81e45aa4
jasonincanada <nirgle@gmail.com>
2019-12-09 00:16:41 -0500

    copy day 5 to 9

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
-import Day08 (part1, part2)
+import Day09 (part1, part2)
 
 main :: IO ()
 main = do
-  input <- readFile "inputs/8.input"
+  input <- readFile "inputs/9-a.input"
   putStrLn $ part1 input
   putStrLn $ part2 input
 
