5441b3e00d481f7881de330792566d92f975a083
jasonincanada <nirgle@gmail.com>
2019-12-09 16:58:26 -0500

    add mode3, dest

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import Day09 (part1, part2)
 
 main :: IO ()
 main = do
-  input <- readFile "inputs/9-a.input"
+  input <- readFile "inputs/9.input"
   putStrLn $ part1 input
   putStrLn $ part2 input
 
